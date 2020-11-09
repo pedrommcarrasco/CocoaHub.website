549737973e899b4bfe94f2c5c18c665d38540fb6
Pedro Carrasco <pedrommcarrasco@gmail.com>
2020-11-09 14:06:33 +0000

    Added News section to NewsList.js

--- src/components/News/NewsList.js
+++ src/components/News/NewsList.js
@@ -3,15 +3,22 @@ import { News } from "models/New";
 
 // TODO: Need to sort & grouped by Date and add Tags Component to each row
 export const NewsList = () => {
-    return News.map((object) => {
-        return <div className={styles.row} id= {object.id}>
+    return News.map((section) => {
+      return <div className={styles.section}>
+        <div className={styles.sectionHeader}>
+          {section.date}
+        </div>
+        {section.objects.map ((object) => {
+          return <div className={styles.row} id= {object.id}>
           <div className={styles.rowTitle}>
             {object.title}
           </div>
-          <div className={styles.rowBody}>
+         <div className={styles.rowBody}>
             {object.description}
-          </div>
+         </div>
         </div>
+        })}
+      </div>
     })
 }
 
--- src/models/New.js
+++ src/models/New.js
@@ -1,29 +1,93 @@
 // Dummy News
 export const News = [
     {
-        id: 1,
-        title: "iOS Dev Weekly 481",
-        description: "Read Dave's curated selection of articles, news & tools from this week."
+        date: 'October 20, 2020',
+        objects: [
+            {
+                id: 1,
+                title: "iOS Dev Weekly 481",
+                description: "Read Dave's curated selection of articles, news & tools from this week."
+            },
+            {
+                id: 2,
+                title: "App Store submissions for Big Sur are now open",
+                description: "Along with Bir Sur, you can also submit apps built for iOS 14.2, tvOS 14.2, and watchOS 7.1 using Xcode 12.2 Release Candidate."
+            },
+            {
+                id: 3,
+                title: "App privacy questions available in App Store Connect",
+                description: "“On each app’s product page, users can learn about some of the data types the app may collect, and whether that data is linked to them or used to track them. This information will be required starting December 8, 2020.“"
+            },
+            {
+                id: 4,
+                title: "Swift by Sundell Podcast 85: “Cross-Apple platform development“",
+                description: "Listen to John Sundell and his special guest, James Thomson, talk about how developers can bring their apps to multiple Apple platforms, how and when it can be a good idea to share code across platforms, and much more."
+            },
+            {
+                id: 5,
+                title: "iOS 14.2 is now available",
+                description: "Apple also releases iPadOS 14.2, tvOS 14.2, iPadOS 12.4.9, iOS 12.4.9, macOS Big Sur 11.0.1 Release Candidate and Xcode 12.2 Release Candidate."
+            }
+        ]
     },
     {
-        id: 2,
-        title: "App Store submissions for Big Sur are now open",
-        description: "Along with Bir Sur, you can also submit apps built for iOS 14.2, tvOS 14.2, and watchOS 7.1 using Xcode 12.2 Release Candidate."
+        date: 'October 19, 2020',
+        objects: [
+            {
+                id: 1,
+                title: "iOS Dev Weekly 481",
+                description: "Read Dave's curated selection of articles, news & tools from this week."
+            },
+            {
+                id: 2,
+                title: "App Store submissions for Big Sur are now open",
+                description: "Along with Bir Sur, you can also submit apps built for iOS 14.2, tvOS 14.2, and watchOS 7.1 using Xcode 12.2 Release Candidate."
+            },
+            {
+                id: 3,
+                title: "App privacy questions available in App Store Connect",
+                description: "“On each app’s product page, users can learn about some of the data types the app may collect, and whether that data is linked to them or used to track them. This information will be required starting December 8, 2020.“"
+            },
+            {
+                id: 4,
+                title: "Swift by Sundell Podcast 85: “Cross-Apple platform development“",
+                description: "Listen to John Sundell and his special guest, James Thomson, talk about how developers can bring their apps to multiple Apple platforms, how and when it can be a good idea to share code across platforms, and much more."
+            },
+            {
+                id: 5,
+                title: "iOS 14.2 is now available",
+                description: "Apple also releases iPadOS 14.2, tvOS 14.2, iPadOS 12.4.9, iOS 12.4.9, macOS Big Sur 11.0.1 Release Candidate and Xcode 12.2 Release Candidate."
+            }
+        ]
     },
     {
-        id: 3,
-        title: "App privacy questions available in App Store Connect",
-        description: "“On each app’s product page, users can learn about some of the data types the app may collect, and whether that data is linked to them or used to track them. This information will be required starting December 8, 2020.“"
-    },
-    {
-        id: 4,
-        title: "Swift by Sundell Podcast 85: “Cross-Apple platform development“",
-        description: "Listen to John Sundell and his special guest, James Thomson, talk about how developers can bring their apps to multiple Apple platforms, how and when it can be a good idea to share code across platforms, and much more."
-    },
-    {
-        id: 5,
-        title: "iOS 14.2 is now available",
-        description: "Apple also releases iPadOS 14.2, tvOS 14.2, iPadOS 12.4.9, iOS 12.4.9, macOS Big Sur 11.0.1 Release Candidate and Xcode 12.2 Release Candidate."
-    }
-    
+        date: 'October 18, 2020',
+        objects: [
+            {
+                id: 1,
+                title: "iOS Dev Weekly 481",
+                description: "Read Dave's curated selection of articles, news & tools from this week."
+            },
+            {
+                id: 2,
+                title: "App Store submissions for Big Sur are now open",
+                description: "Along with Bir Sur, you can also submit apps built for iOS 14.2, tvOS 14.2, and watchOS 7.1 using Xcode 12.2 Release Candidate."
+            },
+            {
+                id: 3,
+                title: "App privacy questions available in App Store Connect",
+                description: "“On each app’s product page, users can learn about some of the data types the app may collect, and whether that data is linked to them or used to track them. This information will be required starting December 8, 2020.“"
+            },
+            {
+                id: 4,
+                title: "Swift by Sundell Podcast 85: “Cross-Apple platform development“",
+                description: "Listen to John Sundell and his special guest, James Thomson, talk about how developers can bring their apps to multiple Apple platforms, how and when it can be a good idea to share code across platforms, and much more."
+            },
+            {
+                id: 5,
+                title: "iOS 14.2 is now available",
+                description: "Apple also releases iPadOS 14.2, tvOS 14.2, iPadOS 12.4.9, iOS 12.4.9, macOS Big Sur 11.0.1 Release Candidate and Xcode 12.2 Release Candidate."
+            }
+        ]
+    }      
 ]
